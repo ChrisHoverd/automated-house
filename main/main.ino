3bfc9ae0ad5e848f35c0a1009bc90c1360ff5b56
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2020-10-24 20:57:21 -0400

    Initial buzzer and button commit

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int buttonPin = 52;
+const int buzzerPin = 8;
+const int ledPin;
+double currentTime;
+double lastTime;
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  checkButtonState();
+  
+  
+}
 
+void checkButtonState(){
+  if(digitalRead(buttonPin) == LOW){
+    Serial.println("Button Pushed");
+    buzzerOn();
+    //tone(buzzerPin, 500, 2000);
+    }
+  }
+ 
+
+void buzzerOn(){
+  tone(buzzerPin, 500, 2000);
+  Serial.println("Buzzer has started");
 }
