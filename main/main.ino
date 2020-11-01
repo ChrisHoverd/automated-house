c9af20bc9d4ce2f84e9a88b40ef2ec0a8844d2c5
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2020-11-01 00:48:02 -0400

    Added alarm states and alarm on/off

--- main/main.ino
+++ main/main.ino
@@ -1,35 +1,64 @@
 
-const int buttonPin = 52;
-const int buzzerPin = 8;
-const int ledPin;
-double currentTime;
-double lastTime;
+#define buttonPin 2
+#define buzzerPin 52
+#define greenLED 50
+#define redLED 51
 
-
+unsigned long ms_from_start = 0;
+unsigned long ms_previous_read_redLED = 0;
+unsigned long redLED_interval = 250;
+int greenLED_state = 0;
+int redLED_state = 0;
+volatile int alarm_state = 0;
 void setup() {
-  
+
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
-  
+
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, FALLING);
+
+  digitalWrite(greenLED, HIGH);
 }
 
 void loop() {
-  checkButtonState();
-  
-  
+
+  if(alarm_state == 0){ //turns off alarm
+    greenLED_state = 1;
+    redLED_state = 0;
+    digitalWrite(greenLED, greenLED_state);
+    digitalWrite(redLED, redLED_state);
+    noTone(buzzerPin);
+  }
+  else{ //turns on buzzer alarm and red strobe LED
+    tone(buzzerPin, 500);
+    redLEDAlert();
+  }
+
 }
 
-void checkButtonState(){
-  if(digitalRead(buttonPin) == LOW){
-    Serial.println("Button Pushed");
-    buzzerOn();
-    //tone(buzzerPin, 500, 2000);
+void redLEDAlert() { //Flashes redLED
+  digitalWrite(greenLED, LOW);
+  ms_from_start = millis();
+  if (ms_from_start - ms_previous_read_redLED > redLED_interval) {
+    ms_previous_read_redLED = millis();
+    if (redLED_state == 0) {
+      redLED_state = 1;
     }
+    else {
+      redLED_state = 0;
+    }
+    digitalWrite(redLED, redLED_state);
   }
- 
+}
 
-void buzzerOn(){
-  tone(buzzerPin, 500, 2000);
-  Serial.println("Buzzer has started");
+void buttonISR() { //PushButton ISR changes alarm state to On or Off
+  if (alarm_state == 0){
+    alarm_state = 1;
+  }
+  else {
+    alarm_state = 0;
+  }
 }
