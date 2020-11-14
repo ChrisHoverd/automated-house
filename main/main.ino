b40db7f9e517a589c5a241ba686e368878805d3a
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2020-11-13 21:09:41 -0500

    added lcd screen

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,38 @@
 
+
+
+
 #define buttonPin 2
 #define buzzerPin 52
 #define greenLED 50
 #define redLED 51
 #define motionSensor 3
 #define flameSensor 18
-#define tempSensor A0
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+const byte rows = 4;
+const byte cols = 3;
+
+char keys[rows][cols] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+byte rowPins[rows] = {26, 27, 28, 29};
+byte colPins[cols] = {30, 31, 32};
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols);
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 
 unsigned long ms_from_strobe_start = 0;
 unsigned long ms_previous_read_strobe = 0;
 unsigned long redLED_interval = 250;
+unsigned long ms_from_lcd_start = 0;
+unsigned long ms_previous_read_lcd = 0;
+unsigned long lcd_interval = 500;
 int greenLED_state = 0;
 int redLED_state = 0;
 
@@ -17,16 +40,14 @@ volatile int alarm_state = 0;
 volatile int motion_sensor_1_state = 0;
 volatile int flame_sensor_1_state =0;
 
-int tempInput = 0
-double temp = 0;
-
 void setup() {
-
+  lcd.init();  //initialize the lcd
+  lcd.backlight();  //open the backlight 
+  
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(motionSensor, INPUT);
-  pinMode(tempSensor, INPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(redLED, OUTPUT);
   
@@ -39,16 +60,19 @@ void setup() {
 
 void loop() {
 
-  tempInput = analogRead(tempSensor);
-  if(alarm_state == 0){ //turns off alarm
+  
+  if(alarm_state == 0){ //no alarm state
+    displayHomeScreen();
     greenLED_state = 1;
     redLED_state = 0;
     motion_sensor_1_state = 0;
+    flame_sensor_1_state =0;
     digitalWrite(greenLED, greenLED_state);
     digitalWrite(redLED, redLED_state);
     noTone(buzzerPin);
   }
   else{ //turns on buzzer alarm and red strobe LED
+    displayAlarmScreen();
     tone(buzzerPin, 500);
     redLEDStrobe();
   }
@@ -87,10 +111,33 @@ void motion_sensor_1_ISR(){ //triggered on motion, raises alarm
   }
 }
 
-void flame_sensor_1_ISR() {
+void flame_sensor_1_ISR() { //triggered on flame, raises alarm
   flame_sensor_1_state = 1;
   Serial.println("Flame Sensor Triggered");
   if (alarm_state == 0){
     alarm_state = 1;
+  }
+}
+
+void displayHomeScreen(){
+  ms_from_lcd_start = millis();
+  if (ms_from_lcd_start - ms_previous_read_lcd > lcd_interval) {
+    lcd.clear();
+    ms_previous_read_lcd = millis();
+    lcd.setCursor ( 0, 0 );            
+    lcd.print("Hello, Mr. Hoverd"); 
+    lcd.setCursor ( 0, 1 );           
+    lcd.print("System Active"); 
+    }
+  }
+  
+
+void displayAlarmScreen(){
+  ms_from_lcd_start = millis();
+  if (ms_from_lcd_start - ms_previous_read_lcd > lcd_interval) {
+    ms_previous_read_lcd = millis();
+  lcd.clear();
+  lcd.setCursor ( 0, 1 );            
+  lcd.print("!!ALARM TRIGGERED!!"); 
   }
 }
